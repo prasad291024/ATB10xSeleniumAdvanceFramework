bfa04edca022fd45ae31876e55a005e3c50a30e7
prasad291024 <prasadvaliv1992@gmail.com>
2025-05-04 16:08:09 +0530

    BDD Cucumber

---
+++ src/test/java/com/prasad_v/runner/CucumberRunnerTests.java
@@ -0,0 +1,15 @@
+package com.prasad_v.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(tags = "",
+        features = {"src/test/resources/features/VWOLogin.feature"},
+        glue = {"com.thetestingacademy.definitions"},
+        plugin = { "pretty", "html:target/cucumber-reports.html"}
+)
+
+public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
+
+}
